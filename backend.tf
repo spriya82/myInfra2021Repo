758383b95af99bbd4b73e7b7c17a359bee70eed3
spriya82 <111626937+spriya82@users.noreply.github.com>
2022-10-14 10:50:55 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "priya-tf-bucket"
     key = "main"
     region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "priyatftable"
   }
 }
